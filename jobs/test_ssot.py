7fce738cf3d357d91a88411a99f8b76020f755f2
Denis Battistini <battis.denis@gmail.com>
2024-08-11 13:22:37 +0200

    test

--- jobs/test_ssot.py
+++ jobs/test_ssot.py
@@ -48,9 +48,9 @@ class ExampleDataSource(DataSource, Job):
 
     source_data = JSONVar()
 
-    def run(self, dryrun, memory_profiling, source_data, *args, **kwargs):
-        self._data = source_data
-        super().run(False, memory_profiling, args, kwargs)
+    def __init__(self, *args, data, **kwargs):
+        super().__init__(*args, **kwargs)
+        self._data = data
 
     def load_source_adapter(self):
         self.source_adapter = MySSoTRemoteAdapter(data=self._data)
