55ad37768e72530598b196f3644589dd27e976a5
Denis Battistini <battis.denis@gmail.com>
2024-08-15 17:16:58 +0200

    test

--- jobs/diffsync/models/base.py
+++ jobs/diffsync/models/base.py
@@ -72,10 +72,14 @@ class VMInterface(NautobotModel):
     _attributes = (
         "status__name",
     )
+    _children = (
+        "ip_addresses",
+    )
 
     name: str
     virtual_machine__name: str
     status__name: str
+    ip_addresses: Optional[List["IPAddress"]] = []
 
 
 class IPAddress(NautobotModel):
@@ -89,64 +93,62 @@ class IPAddress(NautobotModel):
     )
     _attributes = (
         "status__name",
-        "vm_interfaces",
     )
 
     host: str
     mask_length: str
     status__name: str
-    vm_interfaces: Optional[List[str]] = list()
 
-    @classmethod
-    def create(cls, diffsync, ids, attrs):
-        """
-            Create IPAddress object in Nautobot.
+    # @classmethod
+    # def create(cls, diffsync, ids, attrs):
+    #     """
+    #         Create IPAddress object in Nautobot.
 
-            It will create the parent prefix from the provided host and netmask_length if it doesn't already exists
-            It will set the virtual machine's primary_ip4 if attr 'is_primary' is present and set to True
-            It will create an interface to IP association if the object:
-            - has both virtual_machine and interface attributes provided
-            - both objects exists in nautobot database
-        """
+    #         It will create the parent prefix from the provided host and netmask_length if it doesn't already exists
+    #         It will set the virtual machine's primary_ip4 if attr 'is_primary' is present and set to True
+    #         It will create an interface to IP association if the object:
+    #         - has both virtual_machine and interface attributes provided
+    #         - both objects exists in nautobot database
+    #     """
 
-        _virtual_machine = attrs["virtual_machine"]
-        _vm_interface = attrs["vm_interface"]
+    #     _virtual_machine = attrs["virtual_machine"]
+    #     _vm_interface = attrs["vm_interface"]
 
-        nb_interface = None
-        if attrs["virtual_machine"] and attrs["interface"]:
-            try:
-                nb_interface = OrmVMInterface.objects.get(name=_vm_interface, device__name=_virtual_machine)
-            except OrmVMInterface.DoesNotExist:
-                diffsync.job.logger.warning(f"{_virtual_machine} missing interface {_vm_interface} to assign {ids['address']}")
+    #     nb_interface = None
+    #     if attrs["virtual_machine"] and attrs["interface"]:
+    #         try:
+    #             nb_interface = OrmVMInterface.objects.get(name=_vm_interface, device__name=_virtual_machine)
+    #         except OrmVMInterface.DoesNotExist:
+    #             diffsync.job.logger.warning(f"{_virtual_machine} missing interface {_vm_interface} to assign {ids['address']}")
 
-        nb_status = Status.objects.get(name=attrs["status"]),
-        network_object = ipaddress.IPv4Network(f"{attrs['address']}/{attrs['mask_length']}", strict=False)
-        nb_prefix, created = OrmPrefix.objects.get_or_create(
-            prefix=network_object.with_prefixlen,
-            defaults={
-                "prefix": network_object.with_prefixlen,
-                "namespace__name": "Global",
-                "status__name": nb_status
-            }
-        )
-        if created:
-            diffsync.job.logger.info(f"Prefix {network_object.with_prefixlen} created")
+    #     nb_status = Status.objects.get(name=attrs["status"]),
+    #     network_object = ipaddress.IPv4Network(f"{attrs['address']}/{attrs['mask_length']}", strict=False)
+    #     nb_prefix, created = OrmPrefix.objects.get_or_create(
+    #         prefix=network_object.with_prefixlen,
+    #         defaults={
+    #             "prefix": network_object.with_prefixlen,
+    #             "namespace__name": "Global",
+    #             "status__name": nb_status
+    #         }
+    #     )
+    #     if created:
+    #         diffsync.job.logger.info(f"Prefix {network_object.with_prefixlen} created")
 
-        nb_ipaddress = OrmIPAddress.objects.create(
-            address=ids["address"],
-            status=nb_status,
-        )
-        if nb_interface:
-            mapping = OrmIPAddressToInterface.objects.create(ip_address=nb_ipaddress, interface=nb_interface)
-            mapping.validated_save()
-            diffsync.job.logger.info(f"IP {nb_ipaddress.address} assigned to interfacez {_vm_interface} on virtual machine {_virtual_machine}")
-        nb_ipaddress.validated_save()
+    #     nb_ipaddress = OrmIPAddress.objects.create(
+    #         address=ids["address"],
+    #         status=nb_status,
+    #     )
+    #     if nb_interface:
+    #         mapping = OrmIPAddressToInterface.objects.create(ip_address=nb_ipaddress, interface=nb_interface)
+    #         mapping.validated_save()
+    #         diffsync.job.logger.info(f"IP {nb_ipaddress.address} assigned to interfacez {_vm_interface} on virtual machine {_virtual_machine}")
+    #     nb_ipaddress.validated_save()
 
-        if attrs["virtual_machine"] and attrs["is_primary"]:
-            device = OrmVirtualMachine.objects.get(
-                name=_virtual_machine,
-            )
-            device.primary_ip4 = OrmIPAddress.objects.get(address=ids["address"])
-            device.save()
+    #     if attrs["virtual_machine"] and attrs["is_primary"]:
+    #         device = OrmVirtualMachine.objects.get(
+    #             name=_virtual_machine,
+    #         )
+    #         device.primary_ip4 = OrmIPAddress.objects.get(address=ids["address"])
+    #         device.save()
 
-        return super().create(ids=ids, diffsync=diffsync, attrs=attrs)
+    #     return super().create(ids=ids, diffsync=diffsync, attrs=attrs)
