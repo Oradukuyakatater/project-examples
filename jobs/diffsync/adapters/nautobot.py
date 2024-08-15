a29d9582cc4411f45830b56a36c577f6f354be12
Denis Battistini <battis.denis@gmail.com>
2024-08-15 17:26:33 +0200

    test

--- jobs/diffsync/adapters/nautobot.py
+++ jobs/diffsync/adapters/nautobot.py
@@ -10,8 +10,17 @@ from nautobot_ssot.contrib import NautobotAdapter
 class VirtualMachineNautobotAdapter(NautobotAdapter):
     """DiffSync adapter for Nautobot."""
 
+    # prefix = PrefixModel
     virtual_machine = VirtualMachineModel
-    top_level = ("virtual_machine",)
+    vm_interface = VMInterfaceModel
+    ip_address = IPAddressModel
+
+    top_level = (
+        # "prefix",
+        "virtual_machine",
+        "vm_interface",
+        "ip_address",
+    )
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
